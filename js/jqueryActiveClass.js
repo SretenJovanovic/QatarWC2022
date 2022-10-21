f5b2d09400e2dd4582042011598af0a3be831ad7
SretenJovanovic <sreten93jovanovic@gmail.com>
2022-10-21 12:42:32 +0200

    prepravljenja funkcija za dodavanje active class u navigaciji

--- js/jqueryActiveClass.js
+++ js/jqueryActiveClass.js
@@ -1,9 +1,22 @@
 // funkcija za dodavanje is-active klase ukoliko je url i href linka identican
-$(function($) {
+// Prepisana funkcija sa weba
+// $(function($) {
+//     let url = window.location.href;
+//     $('nav ul li a').each(function() {
+//       if (this.href === url) {
+//         $(this).closest('li').addClass('is-active');
+//       }
+//     });
+//   });
+
+// Nacin na koji sam uspeo da resim
+  (function () {
     let url = window.location.href;
-    $('nav ul li a').each(function() {
-      if (this.href === url) {
-        $(this).closest('li').addClass('is-active');
+    let links = document.querySelectorAll('nav ul li a');
+    links.forEach((link) => {
+      if (link.href === url) {
+        link.parentElement.classList.add('is-active');
+        console.log(link.parentElement.classList);
       }
     });
-  });
+  }());
